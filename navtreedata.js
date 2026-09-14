b364af01cc7c9f7815ac82286976619e8c6eaf4d
Kirbyrawr <devkirbyrawr@gmail.com>
2026-09-14 22:02:39 +0200

    Docs for deki-nodegraph from deki-editor f449cd3

--- navtreedata.js
+++ navtreedata.js
@@ -24,7 +24,12 @@
 */
 var NAVTREE =
 [
-  [ "deki-nodegraph", "index.html", ]
+  [ "deki-nodegraph", "index.html", [
+    [ "Changelog", "index.html#autotoc_md0", [
+      [ "0.15.0", "index.html#autotoc_md1", null ],
+      [ "Added", "index.html#autotoc_md2", null ]
+    ] ]
+  ] ]
 ];
 
 var NAVTREEINDEX =
